c9cd327a542eb1f5c6b7790701700e3680910d1b
Gabriel <gabriel.pedreiro@hotmail.com>
2024-02-22 13:07:27 -0300

    Inicia atividades do curso 'Consumindo API'

--- screenMatchComObjetos/src/Principal/PrincipalComBusca.java
+++ screenMatchComObjetos/src/Principal/PrincipalComBusca.java
@@ -5,16 +5,23 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner read = new Scanner(System.in);
+        System.out.println("- Digite o nome de um filme: ");
+        var busca = read.nextLine(); // O var tipa a variáveld e acordo com o que ela recebe, como se criasse ela sem tipo algum e de acordo com o que ela recebesse, seria definido seu tipo.
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=d1fb1433";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder() //Criando a reqquisição e passando endereço.
-                .uri(URI.create("https://www.omdbapi.com/?t=minions&apikey=d1fb1433"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client  //Armazenando a resposta da requisição.
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body()); //A resposta da requisição possui um corpo, que é o que vem no JSON, por isso colocamos o ".body" alí.
+
     }
 }
