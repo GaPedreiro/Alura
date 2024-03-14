eb46b01e915b4a0af80f492a7a31651a8df67113
Gabriel <gabriel.pedreiro@hotmail.com>
2024-03-14 18:08:22 -0300

    Continua aula 1 do curso 'Trabalhando com lambdas'

--- Java Web/Trabalhando com lambdas/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Java Web/Trabalhando com lambdas/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.Service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,20 @@ public class ScreenmatchApplication implements CommandLineRunner { // CommandoLi
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring sem Web");
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=Supernatural&Season=1&apikey=d1fb1433");
+		System.out.println(json);
+
+		// Agora a ideia é transoformar esse json de resposta em um objeto de uma classe. Vamos usar o Jackson pra isso. Ele é semelhante ao Gson.
+
+		/*
+		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		// Nesse exemplo aqui, pode-se verificar que podemos passar qualquer
+		endereço de API para consumir os dados dela, nossa classe ConsumoAPI
+		é versátil e retorna o json de qualquer endereço de API que você apssar aqui,
+		essa no caso retorna a imagem de um café.
+		 */
 	}
 }
---
+++ Java Web/Trabalhando com lambdas/screenmatch/src/main/java/br/com/alura/screenmatch/Service/ConsumoAPI.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+    public String obterDados (String endereco) { // Retorna o JSON da consulta.
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = cliente.send(request, HttpResponse.BodyHandlers.ofString());
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return response.body();
+    }
+}
